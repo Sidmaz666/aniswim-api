f7a8062ae2b5d6d19951a3658ff8d279e65ff00b
sidmaz666 <sidmazumder8@gmail.com>
2023-04-22 12:43:58 +0530

    Adding a Proxy Route 🚀

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ let proxy = corsAnywhere.createServer({
 });
 
 /* Attach our cors proxy to the existing API on the /proxy endpoint. */
-server.get('/proxy/:proxyUrl', (req, res) => {
-  req.url = req.params.proxyUrl;
+server.get('/proxy/:proxyUrl*', (req, res) => {
+  req.url = req.url.replace(/.*\/proxy\//g, '/'); // Strip '/proxy' from the front of the URL, else the proxy won't work.
   proxy.emit('request', req, res);
 });
 

ee28dfd6e2da805ad661399324b1407852ae91a1
sidmaz666 <sidmazumder8@gmail.com>
2023-04-24 08:33:14 +0530

    Removing Proxy Route 😐

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express'); 
 const ani = require('./functions/main')
 const cors = require('cors')
-const corsAnywhere = require('cors-anywhere');
 
 
 const server = express()
@@ -10,18 +9,6 @@ server.use(cors())
 
 const port = process.env.PORT || 3020 
 
-let proxy = corsAnywhere.createServer({
-  originWhitelist: [], // Allow all origins
-  // Do not require any headers.
-  requireHeaders: ['origin','x-requested-with'],
-  removeHeaders: [] // Do not remove any headers.
-});
-
-/* Attach our cors proxy to the existing API on the /proxy endpoint. */
-server.get('/proxy/:proxyUrl*', (req, res) => {
-  req.url = req.url.replace(/.*\/proxy\//g, ''); // Strip '/proxy' from the front of the URL, else the proxy won't work.
-  proxy.emit('request', req, res);
-});
 
 server.get('/', (req,res) => {
   const page = req.query.page || 1

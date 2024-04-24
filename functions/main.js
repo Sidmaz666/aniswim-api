8b26e502128544c534ce9209e238b001bdf567d2
sidmaz666 <sidmazumder8@gmail.com>
2024-04-24 21:05:13 +0530

    fix array to string

--- functions/main.js
+++ functions/main.js
@@ -422,7 +422,8 @@ async function Search(res,{...args}) {
 		}
   	}
   }
-  const search_url = `${SITEURL}/${page_type}.html?keyword=${query}&page=${page}${extra_args}`;
+  const search_url = `${SITEURL}/${page_type}.html?keyword=${query}&page=${page}${extra_args.join("")}`;
+  console.log(search_url)
   try{
     const anime = await Extractor(search_url)
     anime.push({filters:args})
